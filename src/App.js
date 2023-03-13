4afbdb379c756ecd74ec8c4476ea7a75c9a07c84
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2023-03-13 19:39:07 +0700

    Modify client app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
+import Home from './pages/home';
+
+// Our server will run on port 4000
+const socket = io.connect('http://localhost:4000');
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          {/* Homepage */}
+          <Route
+            path='/'
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
